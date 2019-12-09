3d40285a0d4d9578803b15f343f516a296c81528
Fulton <fulchr@dunwoody.edu>
2019-12-09 13:10:30 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace assignment_five
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Create a three object instances of Estimate and output data using toString Method from Estimate class  
+        }
+
+        //Create a class called Estimate
+    }
+}
